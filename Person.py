791cc11a0befaa6cf08ede07712be4e884eb25b7
jiajialing@gmail.com <jiajialing@gmail.com>
2019-10-28 14:14:48 +1100

    first commit

---
+++ Person.py
@@ -0,0 +1,29 @@
+
+import os
+class Person:
+
+    def __init__(self, firstname, lastname, score):
+        self.firstname=firstname
+        self.lastname=lastname
+        self.score=score
+
+    def Pr(self):
+        os.system('cls')
+        print('Student first name is: '+ self.firstname)
+        print('Studnet last name is: ' + self.lastname)
+        print('Student score is: ' + str(self.score))
+               
+             
+    def grade(self):
+        if self.score < 60:
+            self.gradeTAG='fail'
+            print ('grade is fail')
+        elif self.score >= 60 and self.score<=80:
+            self.gradeTAG='pass'
+            print ('grade is pass')
+        elif self.score >80 and self.score<=100:
+            self.gradeTAG='good'
+            print ('grade is good')
+
+
+
