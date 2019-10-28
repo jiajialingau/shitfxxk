41dc7edf7da64ec42c5629f0aec00d3cd0397f5e
jiajialing@gmail.com <jiajialing@gmail.com>
2019-10-29 10:59:13 +1100

    test

--- test.py
+++ test.py
@@ -2,8 +2,17 @@ import os
 mypath = os.getcwd()
 print (mypath)
 
-msg="my name is jiajialing"
-print(msg)
-print("test")
-print("jiajialng")
-os.system('cls')
+# msg="my name is jiajialing"
+# print(msg)
+# print("jiajialng")
+# os.system('cls')
+
+a=input('please input how many loop you want: ')
+a=int(a)
+
+while a>0:
+    print (a)
+    a=a-1
+
+
+    
